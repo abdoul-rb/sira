067f4cbb19154e82d5989c5077b7a43d821b7bf6
abdoul-rb <abdoulrahimbah6@gmail.com>
2025-06-23 23:50:33 +0200

    chore: setup basic tenant route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Dashboard\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 
 require __DIR__ . '/auth.php';
+
+Route::domain('{company}.' . config('app.url'))/* ->middleware(['auth']) */->group(function () {
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+});

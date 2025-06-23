d54d41368e40a580169ea64ed7ed306dc9f9de6b
abdoul-rb <abdoulrahimbah6@gmail.com>
2025-06-24 00:05:48 +0200

    feat: add companies data

---
+++ app/Models/Company.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Company extends Model
+{
+    /** @use HasFactory<\Database\Factories\CompanyFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'uuid',
+        'slug',
+        'name',
+        'email',
+        'phone_number',
+        'website',
+        'active',
+        'logo_path',
+        'address',
+        'city',
+        'zip_code',
+        'country',
+    ];
+
+    protected $casts = [
+        'active' => 'boolean',
+    ];
+
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function (Company $company) {
+            $company->slug = Str::slug($company->name);
+            $company->uuid = Str::uuid();
+        });
+
+        static::updating(function (Company $company) {
+            $company->slug = Str::slug($company->title);
+        });
+    }
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relations
+    |--------------------------------------------------------------------------
+    */
+}

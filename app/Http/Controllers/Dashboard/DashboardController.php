39c305e270c46b8358bd28319af1c9bc8a5d933d
abdoul-rb <abdoulrahimbah6@gmail.com>
2025-09-27 01:23:19 +0200

    feat: add stat in to dashboard

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -6,19 +6,36 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use App\Models\Company;
+use App\Models\Customer;
 use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Cache;
 
 class DashboardController extends Controller
 {
     public function index(Request $request, Company $tenant)
     {
-        $orders = Order::where('company_id', $tenant->id)->orderBy('created_at', 'desc')->take(5)->get();
-        // Log::info("DashboardController::index", ['tenant' => $tenant->slug]);
+        $orders = Cache::remember("dashboard-last-orders-{$tenant->id}", 60 * 60, function () use ($tenant) {
+            return Order::where('company_id', $tenant->id)->orderBy('created_at', 'desc')->take(5)->get();
+        });
 
-        // Maintenant $tenant est un objet Company complet
-        // dd($tenant);
-        return view('dashboard.index', compact('orders', 'tenant'));
+        $customersCount = Customer::where('company_id', $tenant->id)->count();
+
+        $productsCount = Product::where('company_id', $tenant->id)->count();
+        $productsStock = Product::where('company_id', $tenant->id)->sum('stock_quantity');
+
+        $totalSales = Order::where('company_id', $tenant->id)->sum('total_amount');
+
+        $totalOrders = Order::where('company_id', $tenant->id)->count();
+
+        return view('dashboard.index', [
+            'orders' => $orders,
+            'tenant' => $tenant,
+            'customersCount' => $customersCount,
+            'productsCount' => $productsCount,
+            'totalSales' => $totalSales,
+            'totalOrders' => $totalOrders,
+        ]);
     }
 }

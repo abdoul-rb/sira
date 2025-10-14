750507f52493c3384f9f3e25de855338d85cbf38
abdoul-rb <abdoulrahimbah6@gmail.com>
2025-10-14 15:53:19 +0200

    feat: inject tenant in profile route

--- app/Livewire/Profile/Index.php
+++ app/Livewire/Profile/Index.php
@@ -5,9 +5,17 @@ declare(strict_types=1);
 namespace App\Livewire\Profile;
 
 use Livewire\Component;
+use App\Models\Company;
 
 class Index extends Component
 {
+    public Company $tenant;
+    
+    public function mount(Company $tenant)
+    {
+        $this->tenant = $tenant;
+    }
+
     public function render()
     {
         return view('livewire.profile.index')->extends('layouts.dashboard');

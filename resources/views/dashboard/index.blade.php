b8c23c976880cd925a290c4544866bfb12670ae9
abdoul-rb <abdoulrahimbah6@gmail.com>
2025-11-16 22:51:39 +0100

    update dashboard index

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -25,6 +25,8 @@
             </div>
         </div>
 
+        {{-- @dump($currentTenant) --}}
+
         <div class="mt-6 grid grid-cols-12 gap-4">
             <div class="col-span-12 gap-4 lg:col-span-7 space-y-4">
                 <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">

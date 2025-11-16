3b6d80f9222681632807a2c93ca83020b3bb545b
abdoul-rb <abdoulrahimbah6@gmail.com>
2025-11-16 22:48:51 +0100

    update menu

--- resources/views/partials/dashboard/__header-menu.blade.php
+++ resources/views/partials/dashboard/__header-menu.blade.php
@@ -12,14 +12,14 @@
                         {{ __('Tableau de bord') }}
                     </a>
 
-                    <a href="{{ route('dashboard.purchases.index') }}"
-                        class="inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 {{ request()->routeIs('dashboard.purchases.*') ? 'border-indigo-600 text-gray-900' : 'border-transparent' }}">
-                        {{ __('Achats') }}
-                    </a>
-
                     <a href="{{ route('dashboard.products.index') }}"
                         class="inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 {{ request()->routeIs('dashboard.products.*') ? 'border-indigo-600 text-gray-900' : 'border-transparent' }}">
-                        {{ __('Stock') }}
+                        {{ __('Produits') }}
+                    </a>
+
+                    <a href="#"
+                        class="inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 {{ request()->routeIs('dashboard.marketings.*') ? 'border-indigo-600 text-gray-900' : 'border-transparent' }}">
+                        {{ __('Marketing') }}
                     </a>
 
                     <a href="{{ route('dashboard.orders.index') }}"
@@ -29,7 +29,7 @@
 
                     <a href="{{ route('dashboard.customers.index') }}"
                         class="inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 {{ request()->routeIs('dashboard.customers.*') ? 'border-indigo-600 text-gray-900' : 'border-transparent' }}">
-                        {{ __('Clients') }}
+                        {{ __('Contacts') }}
                     </a>
                 </div>
             </div>

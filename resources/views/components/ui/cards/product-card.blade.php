bb93fc0c95c8778203a3fec4f82d4ae6a10a398e
abdoul-rb <abdoulrahimbah6@gmail.com>
2025-11-21 22:57:06 +0100

    update and add dropdown component

--- resources/views/components/ui/cards/product-card.blade.php
+++ resources/views/components/ui/cards/product-card.blade.php
@@ -24,28 +24,22 @@
             </div>
 
             @can('update', $product)
-                <div class="relative ml-auto" x-data="{ dropdownOpen: false }">
-                    <button @click="dropdownOpen = !dropdownOpen" type="button"
-                        class="-m-2.5 block p-2 text-gray-400 hover:text-gray-500 cursor-pointer rounded-md hover:bg-gray-50"
-                        id="options-menu-0-button" aria-expanded="false" aria-haspopup="true"
-                        x-transition:enter="transition ease-out duration-200"
-                        x-transition:enter-start="transform opacity-0 scale-95"
-                        x-transition:enter-end="transform opacity-100 scale-100"
-                        x-transition:leave="transition ease-in duration-75"
-                        x-transition:leave-start="transform opacity-100 scale-100"
-                        x-transition:leave-end="transform opacity-0 scale-95">
-                        <span class="sr-only">Open options</span>
-                        <svg class="h-5 w-5" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor"
-                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z">
-                            </path>
-                        </svg>
-                    </button>
+                <x-ui.dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <button type="button"
+                            class="-m-2.5 block p-2 text-gray-400 hover:text-gray-500 cursor-pointer rounded-md hover:bg-gray-50"
+                            id="options-menu-0-button" aria-expanded="false" aria-haspopup="true">
+                            <span class="sr-only">Open options</span>
+                            <svg class="h-5 w-5" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor"
+                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z">
+                                </path>
+                            </svg>
+                        </button>
+                    </x-slot>
 
-                    <div x-show="dropdownOpen"
-                        class="absolute right-0 z-10 mt-0.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none"
-                        role="menu" aria-orientation="vertical" aria-labelledby="options-menu-0-button" tabindex="-1">
+                    <x-slot name="content">
                         <button type="button" wire:click="edit({{ $product->id }})" @click="dropdownOpen = false"
                             class="w-full block px-3 py-1 text-xs font-medium leading-6 text-gray-900 hover:bg-gray-50 text-left cursor-pointer"
                             role="menuitem" tabindex="-1" id="options-menu-0-item-1">
@@ -58,8 +52,8 @@
                             role="menuitem" tabindex="-1" id="options-menu-0-item-1">
                             Supprimer
                         </button>
-                    </div>
-                </div>
+                    </x-slot>
+                </x-ui.dropdown>
             @endcan
         </div>
 

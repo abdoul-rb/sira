06b6ec8acd54eed4dbf4fbb5bd4a9a3cf2fa7913
abdoul-rb <abdoulrahimbah6@gmail.com>
2025-11-26 20:50:20 +0100

    use current_tenant helper instead auth member

--- resources/views/partials/__header.blade.php
+++ resources/views/partials/__header.blade.php
@@ -47,7 +47,7 @@
                 @endguest
 
                 @auth
-                    <a href="{{ route('dashboard.index', ['tenant' => auth()->user()->member->company]) }}"
+                    <a href="{{ route('dashboard.index', ['tenant' => current_tenant()]) }}"
                         class="group inline-flex items-center justify-center rounded-full py-2 px-6 text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-600 text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600"
                         color="blue" variant="solid">
                         <span>Tableau de bord</span>

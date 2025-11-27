a78ca7ce8f33b88a793516da798d6528db32a445
abdoul-rb <abdoulrahimbah6@gmail.com>
2025-11-27 12:14:21 +0100

    refactor: update all

--- resources/views/dashboard/settings/index.blade.php
+++ resources/views/dashboard/settings/index.blade.php
@@ -87,7 +87,7 @@
                     </x-ui.cards.settings-card-link>
 
                     <x-ui.cards.settings-card-link routeName="dashboard.settings.expenses.index" title="Dépenses"
-                        description="Enregistrez et consultez vos dépenses et charges fixes.">
+                        description="Dépenses/Charges fixes et récurrentes">
                         <x-slot:icon>
                             <svg class="size-6 shrink-0 text-white" data-slot="icon" fill="none" stroke-width="1.5"
                                 stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
@@ -99,17 +99,19 @@
                         </x-slot:icon>
                     </x-ui.cards.settings-card-link>
 
-                    {{-- <x-ui.cards.settings-card-link :route="route('dashboard.settings.shop', ['tenant' => $currentTenant])" title="Boutique"
-                    description="Affichez et mettez à jour les informations de votre boutique.">
-                    <x-slot:icon>
-                        <svg class="size-6 shrink-0 text-white" data-slot="icon" fill="none" stroke-width="1.5"
-                            stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z">
-                            </path>
-                        </svg>
-                    </x-slot:icon>
-                </x-ui.cards.settings-card-link> --}}
+                    {{-- <x-ui.cards.settings-card-link
+                        :route="route('dashboard.settings.shop', ['tenant' => $currentTenant])" title="Boutique"
+                        description="Affichez et mettez à jour les informations de votre boutique.">
+                        <x-slot:icon>
+                            <svg class="size-6 shrink-0 text-white" data-slot="icon" fill="none" stroke-width="1.5"
+                                stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+                                aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z">
+                                </path>
+                            </svg>
+                        </x-slot:icon>
+                    </x-ui.cards.settings-card-link> --}}
                 </div>
             </div>
         </aside>
--- resources/views/partials/__header.blade.php
+++ resources/views/partials/__header.blade.php
@@ -98,9 +98,12 @@
 
                             <hr class="m-2 border-slate-300/40">
 
-                            <a class="block w-full p-2" href="/login" @click="openMenu = false">
-                                Connexion
-                            </a>
+                            @auth
+                                <a href="{{ route('dashboard.index', ['tenant' => current_tenant()]) }}"
+                                    class="inline-flex items-center justify-center rounded-full py-2 px-6 text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-600 text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600">
+                                    <span>Tableau de bord</span>
+                                </a>
+                            @endauth
                         </div>
                     </div>
                 </div>

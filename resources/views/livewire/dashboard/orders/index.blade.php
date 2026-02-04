77c17fa57d14ca26904f84ee3f8d5838c7ab605b
abdoul-rb <abdoulrahimbah6@gmail.com>
2026-02-04 21:54:05 +0100

    feat: update grid card

--- resources/views/livewire/dashboard/orders/index.blade.php
+++ resources/views/livewire/dashboard/orders/index.blade.php
@@ -55,8 +55,19 @@
     <!-- Modal de création de produit -->
     <x-ui.modals.create-order-modal :tenant="$tenant" />
 
-    <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
-        <x-ui.cards.trending-stat label="Nombre de ventes" :value="$orders->count()">
+    <div class="grid grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-4">
+        <x-ui.cards.trending-stat class="col-span-full" label="Montants totals" :value="Number::currency($totalSales, in: 'XOF', locale: 'fr')">
+            <x-slot:icon>
+                <svg class="size-6 text-blue-500" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor"
+                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M12 3v17.25m0 0c-1.472 0-2.882.265-4.185.75M12 20.25c1.472 0 2.882.265 4.185.75M18.75 4.97A48.416 48.416 0 0 0 12 4.5c-2.291 0-4.545.16-6.75.47m13.5 0c1.01.143 2.01.317 3 .52m-3-.52 2.62 10.726c.122.499-.106 1.028-.589 1.202a5.988 5.988 0 0 1-2.031.352 5.988 5.988 0 0 1-2.031-.352c-.483-.174-.711-.703-.59-1.202L18.75 4.971Zm-16.5.52c.99-.203 1.99-.377 3-.52m0 0 2.62 10.726c.122.499-.106 1.028-.589 1.202a5.989 5.989 0 0 1-2.031.352 5.989 5.989 0 0 1-2.031-.352c-.483-.174-.711-.703-.59-1.202L5.25 4.971Z">
+                    </path>
+                </svg>
+                </x-slot>
+        </x-ui.cards.trending-stat>
+
+        <x-ui.cards.trending-stat class="col-span-1" label="Nombre de ventes" :value="$orders->count()">
             <x-slot:icon>
                 <svg class="size-6 text-blue-500" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor"
                     viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
@@ -67,7 +78,7 @@
                 </x-slot>
         </x-ui.cards.trending-stat>
 
-        <x-ui.cards.trending-stat label="Crédits" :value="$creditsOrdersCount">
+        <x-ui.cards.trending-stat class="col-span-1" label="Crédits" :value="$creditsOrdersCount">
             <x-slot:icon>
                 <svg class="size-6 text-blue-500" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor"
                     viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
@@ -78,16 +89,6 @@
                 </x-slot>
         </x-ui.cards.trending-stat>
 
-        <x-ui.cards.trending-stat label="Montants totals" :value="Number::currency($totalSales, in: 'XOF', locale: 'fr')">
-            <x-slot:icon>
-                <svg class="size-6 text-blue-500" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor"
-                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M12 3v17.25m0 0c-1.472 0-2.882.265-4.185.75M12 20.25c1.472 0 2.882.265 4.185.75M18.75 4.97A48.416 48.416 0 0 0 12 4.5c-2.291 0-4.545.16-6.75.47m13.5 0c1.01.143 2.01.317 3 .52m-3-.52 2.62 10.726c.122.499-.106 1.028-.589 1.202a5.988 5.988 0 0 1-2.031.352 5.988 5.988 0 0 1-2.031-.352c-.483-.174-.711-.703-.59-1.202L18.75 4.971Zm-16.5.52c.99-.203 1.99-.377 3-.52m0 0 2.62 10.726c.122.499-.106 1.028-.589 1.202a5.989 5.989 0 0 1-2.031.352 5.989 5.989 0 0 1-2.031-.352c-.483-.174-.711-.703-.59-1.202L5.25 4.971Z">
-                    </path>
-                </svg>
-                </x-slot>
-        </x-ui.cards.trending-stat>
     </div>
 
     <!-- Filters -->

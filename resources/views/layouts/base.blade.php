19788bf9496b0ea3fa47dba4437a7c412917e30a
abdoul-rb <abdoulrahimbah6@gmail.com>
2026-02-05 16:51:54 +0100

    update apple meta

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -15,7 +15,7 @@
 
     <!-- PWA Meta Tags -->
     <meta name="theme-color" content="#14b8a6">
-    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="apple-mobile-web-app-title" content="{{ config('app.name') }}">
 

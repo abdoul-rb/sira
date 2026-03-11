8b50feb8f893d3cb9ef7f12412442f08dae8e48f
abdoul-rb <abdoulrahimbah6@gmail.com>
2026-03-11 06:25:15 +0100

    enhance phone input in modal

--- resources/views/livewire/dashboard/customers/index.blade.php
+++ resources/views/livewire/dashboard/customers/index.blade.php
@@ -172,7 +172,7 @@
             </div> --}}
         </div>
 
-        <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <ul role="list" class="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3">
             @forelse($customers as $customer)
                 <x-ui.cards.customer-details :customer="$customer" />
 
